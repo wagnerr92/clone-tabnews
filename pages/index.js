c02ae160e4ba9ba906a2cc130984ddf5a56c4401
Wagner Rodrigues <126831621+wagnerr92@users.noreply.github.com>
2023-07-14 21:29:11 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Bárbara eu amo você, se você me ama da uma risadinha! 😀</h1>
+function Home() {
+  return <h1>Bárbara eu amo você, se você me ama da uma risadinha! 😀</h1>;
 }
 
 export default Home;
